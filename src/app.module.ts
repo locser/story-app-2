f5ff11578b16c5c6f98ac103cab9c8e256c455a1
Pham Huu Loc <ploc280301@gmail.com>
2023-07-19 15:57:06 +0700

    redis bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+// import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConversationModule } from './conversation/conversation.module';
@@ -11,10 +12,31 @@ import { UserModule } from './user/user.module';
 import { Oauth } from './oauth/entities/oauth.entity';
 import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { config } from 'dotenv';
+import { RedisStore, redisStore } from 'cache-manager-redis-store';
+// import { RedisStore } from 'cache-manager-redis-store';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as cacheManager from 'cache-manager';
+// import * as redisStore from 'cache-manager-redis-store';
+config();
 
 @Module({
   imports: [
-    ConversationModule,
+    CacheModule.registerAsync({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      useFactory: async () => {
+        return {
+          store: await redisStore({
+            url: `redis://:${process.env.CONFIG_REDIS_PASSWORD}@${process.env.CONFIG_REDIS_HOST}:${process.env.CONFIG_REDIS_PORT}/${process.env.CONFIG_REDIS_DB}`,
+          }),
+        };
+      },
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -26,12 +48,20 @@ import { AuthModule } from './auth/auth.module';
       // synchronize: true,
       // autoLoadEntities: true,
     }),
+
+    ConversationModule,
     MessageModule,
     OauthModule,
     UserModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
